a75dc2e9fd5754ae6b0f2f4b1b9e10e05632dced
Weuller Jr. Souza <weuller9505@live.com>
2018-10-21 14:25:10 -0200

    Validação do Link

--- js/script.js
+++ js/script.js
@@ -2,16 +2,27 @@ const btnDownload = document.getElementById('getImage');
 var imageURL = document.getElementById('imgURL');
 var baseURL = 'https://instagram.com/p/***/media/?size=l';
 
-function convertLink () {
-    let imgURL = imageURL.value;
-    let startCode = imgURL.indexOf('com/p/') + 6;
-    let endCode = imgURL.indexOf('/', startCode);
-    let CODE = imgURL.substring(startCode, endCode);
+function convertLink (link) {
+    let startCode = link.indexOf('com/p/') + 6;
+    let endCode = link.indexOf('/', startCode);
+    let CODE = link.substring(startCode, endCode);
 
     let LINK = baseURL.replace(/\*\*\*/i, CODE);
     return LINK;
 }
 
+function validate (link) {
+    if (link.includes('instagram.com/p/')) {
+        return true;
+    }
+    return false;
+}
+
 btnDownload.addEventListener('click', () => {
-    location.href = convertLink();
+    if (validate(imageURL.value)) {
+        imageURL.style.borderColor = 'none';
+        location.href = convertLink(imageURL.value);
+    } else {
+        imageURL.style.borderColor = 'red';
+    }
 });
